f1f59f63be0c3a501cde7e240a22a0c51676dbec
Abdullah Javaid <cheema1140@gmail.com>
2021-11-25 07:08:23 -0800

    Assnmnt#3, donnnneee !!!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Hellooworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Sir OOP!");
         }
     }
 }
